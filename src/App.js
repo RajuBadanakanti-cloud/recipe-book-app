15f87b9be25350854253ba71987602730b5a6ce7
Raju Rj <rajubadanakanti166@gmail.com>
2025-07-20 15:42:27 +0530

    Fix: localStorage bug, routing, and homepage setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import {BrowserRouter as Routers, Routes,Route,} from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeBookHome from './components/RecipeBookHome';
 import RecipesList from './components/RecipesList'
 import RecipeDetailedView from './components/RecipeDetailedView';
 import NotFound from './components/NotFound';
 const App = () => (
-<Routers>
+<Router>
   <Routes>
     <Route exact path='/' element={<RecipeBookHome/>}/>
     <Route exact path='/recipes' element={<RecipesList/>}/>
     <Route exact path='/recipe/:id' element={<RecipeDetailedView/>}/>
     <Route path="/not-found" element={<NotFound/>}/>
   </Routes>
-</Routers>
+</Router>
 )
 
 export default App;
