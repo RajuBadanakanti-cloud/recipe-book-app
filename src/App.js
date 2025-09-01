424c7737be3217f273c029b56f4ea3564db6280e
Raju Rj <rajubadanakanti166@gmail.com>
2025-09-01 15:04:41 +0530

    Polished

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import {HashRouter, Routes,Route} from 'react-router-dom'
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
 import RecipeBookHome from './components/RecipeBookHome';
 import RecipesList from './components/RecipesList'
 import RecipeDetailedView from './components/RecipeDetailedView';
 import NotFound from './components/NotFound';
 const App = () => (
-<HashRouter > {/* This for app github lives with in built routes-# */}
+<BrowserRouter > {/* This for app github lives with in built routes-# */}
   <Routes>
     <Route exact path='/' element={<RecipeBookHome/>}/>
     <Route exact path='/recipes' element={<RecipesList/>}/>
     <Route exact path='/recipe/:id' element={<RecipeDetailedView/>}/>
     <Route path="/not-found" element={<NotFound/>}/>
   </Routes>
-</HashRouter>
+</BrowserRouter>
 )
 
 export default App;
